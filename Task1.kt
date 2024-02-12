4c23a42ad09c44efcc2a713c8a66dd643ebf8df8
Ali Ozdemir <100178366+aliozdemirao@users.noreply.github.com>
2024-02-12 23:27:48 +0300

    Task1

---
+++ Task1.kt
@@ -0,0 +1,146 @@
+package com.aliozdemir.kotlindersleri.degiskenler
+
+fun main() {
+
+    val district = "San Francisco"
+    println("District: $district")
+
+    val continent = "North America"
+    println("Continent: $continent")
+
+    val fax = "212-693-2377"
+    println("Fax: $fax")
+
+    val postalCode = "95008"
+    println("Postal Code: $postalCode")
+
+    var instagramAddress = "johndoe"
+    println("Instagram Address: @$instagramAddress")
+
+    val departmentWorked = "Mobile Development"
+    println("Department Worked: $departmentWorked")
+
+    var productQuantity = 100
+    println("Product Quantity: $productQuantity")
+
+    val customerSurname = "Doe"
+    println("Customer Surname: $customerSurname")
+
+    var paymentAmount = 1500.0
+    println("Payment Amount: $$paymentAmount")
+
+    val dateOfBirth = "1990-05-15"
+    println("Date of Birth: $dateOfBirth")
+
+    var debt = 200.0
+    println("Debt: $$debt")
+
+    val maritalStatus = "Married"
+    println("Marital Status: $maritalStatus")
+
+    var videoComment = "Great video!"
+    println("Video Comment: $videoComment")
+
+    val paymentTime = "12:30 PM"
+    println("Payment Time: $paymentTime")
+
+    var eftAmount = 500.0
+    println("EFT Amount: $$eftAmount")
+
+    var soldQuantity = 50
+    println("Sold Quantity: $soldQuantity")
+
+    val phoneModel = "Samsung Galaxy S24"
+    println("Phone Model: $phoneModel")
+
+    val magazineName = "TechReview"
+    println("Magazine Name: $magazineName")
+
+    val publicationDate = "2024-02-07"
+    println("Publication Date: $publicationDate")
+
+    var amountOfRaise = 100.0
+    println("Amount of Raise: $amountOfRaise%")
+
+    val numberOfFlats = 20
+    println("Number of Flats: $numberOfFlats")
+
+    val latitude = 37.7749
+    println("Latitude: $latitude")
+
+    val longitude = -122.4194
+    println("Longitude: $longitude")
+
+    var mealName = "Spaghetti Bolognese"
+    println("Meal Name: $mealName")
+
+    var productPrice = 29.99
+    println("Product Price: $$productPrice")
+
+    val company = "Google"
+    println("Company: $company")
+
+    var videoName = "Introduction to Kotlin"
+    println("Video Name: $videoName")
+
+    val musicDuration = 180
+    println("Music Duration: $musicDuration seconds")
+
+    val venueRating = 4.5
+    println("Venue Rating: $venueRating")
+
+    var fileName = "document.txt"
+    println("File Name: $fileName")
+
+    val imageFormat = "JPEG"
+    println("Image Format: $imageFormat")
+
+    val color = "Blue"
+    println("Color: $color")
+
+    val colorCode = "#0000FF"
+    println("Color Code: $colorCode")
+
+    val computerModel = "Dell XPS 13"
+    println("Computer Model: $computerModel")
+
+    val screenSize = 13.3
+    println("Screen Size: $screenSize inch")
+
+    val usageTime = 360
+    println("Usage Time: $usageTime days")
+
+    val pressure = 100
+    println("Pressure: $pressure atm")
+
+    var eventDay = "2024-03-15"
+    println("Event Day: $eventDay")
+
+    val paymentDay = "2024-02-10"
+    println("Payment Day: $paymentDay")
+
+    val travelDepartureDate = "2024-04-01"
+    println("Travel Departure Date: $travelDepartureDate")
+
+    val neighborhoodName = "Santa Clara"
+    println("Neighborhood Name: $neighborhoodName")
+
+    var busLine = "Bus 42"
+    println("Bus Line: $busLine")
+
+    val usedMinutes = 120
+    println("Used Minutes: $usedMinutes")
+
+    val trackingNumber = "ABC123456789"
+    println("Tracking Number: $trackingNumber")
+
+    val couponDuration = 7
+    println("Coupon Duration: $couponDuration days")
+
+    var couponCode = "SAVE20"
+    println("Coupon Code: $couponCode")
+
+    var invoiceDate = "2024-02-07"
+    println("Invoice Date: $invoiceDate")
+
+}
